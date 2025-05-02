aef5d75762be27379529a008b7298ef0c19fc98f
Luis Aviles <123853326+lavil014@users.noreply.github.com>
2025-05-01 20:54:13 -0600

    feat(script): Js files deleted from my project

--- Js/formHandler.js
+++
@@ -1,8 +0,0 @@
-export const resetForm = (form,submitedEmail,submitedName,submiteduserGitHub)=> {
-    
-  form.reset();
-  submitedEmail.value = '';
-  submitedName.value = '';
-  submiteduserGitHub.value = '';
-  isImageUploaded = false; 
-}
